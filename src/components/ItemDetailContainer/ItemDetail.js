41c7d517d64f0844645026b69145dbb122ccad9d
Juanma19R <juan.ruggeri@hotmail.com>
2022-03-31 11:23:16 -0300

    Se agregaron iconos y estilos a el ultimo desafio

--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -1,19 +1,29 @@
 import './ItemDetail.scss'
 import ItemCount from '../ItemCount/ItemCount';
+import HalfRating from '../HalfRating/HalfRating';
+import FloatingActionButtonSize from '../FavButton/FavButton'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
 const ItemDetail = ({item}) => {
     const {name, pictureUrl, price, detail, stock} = item;
     return (
         <div className="ItemDetail-container">
             <div className="Item-container">
-                <div>
+                <div className='img-container'>
                     <img src={pictureUrl} alt=''></img>
                 </div>
                 <div className='ItemDetail-details'>
-                    <h2 className='ItemDetail-title'>{name}</h2>
+                    <h2 className='ItemDetail-title'>{name}</h2>             
                     <span className='ItemDetail-price'><b>$ {price}</b></span>
+                    <div className='Rating-Fav-container'>
+                        <HalfRating /> 
+                        <FloatingActionButtonSize />
+                    </div>
                     <p>{detail}</p>
                     <ItemCount stock={stock} initial={1} />
+                    <div className='send-container'>
+                        <p><LocalShippingIcon color='primary'/> ¡Envios a todo el país!</p>
+                    </div>
                 </div>
         </div>
         </div>
